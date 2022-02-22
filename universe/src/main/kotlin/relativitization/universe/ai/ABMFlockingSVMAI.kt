fe0a11803a0d63226df6da9d4c68409f9ba161ce
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-02-22 17:57:19 +0800

    Add ABMFlockingSVMAI

---
+++ universe/src/main/kotlin/relativitization/universe/ai/ABMFlockingSVMAI.kt
@@ -0,0 +1,10 @@
+package relativitization.universe.ai
+
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.commands.Command
+
+object ABMFlockingSVMAI : AI() {
+    override fun compute(universeData3DAtPlayer: UniverseData3DAtPlayer): List<Command> {
+        return listOf()
+    }
+}
