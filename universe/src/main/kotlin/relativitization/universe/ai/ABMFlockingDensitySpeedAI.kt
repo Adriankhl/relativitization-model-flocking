a5e3b32306f673baef7456b228e5a55a74a599a6
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-05-06 21:12:03 +0800

    Add ABMFlockingDensitySpeedAI

--- universe/src/main/kotlin/relativitization/universe/ai/ABMFlockingDensitySpeedAI.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/ABMFlockingDensitySpeedAI.kt
@@ -1,28 +1,45 @@
 package relativitization.universe.ai
 
-import relativitization.universe.data.PlayerData
 import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.commands.ABMFlockingChangeVelocityCommand
 import relativitization.universe.data.commands.Command
 import relativitization.universe.data.components.abmFlockingData
-import relativitization.universe.maths.physics.Intervals
 import relativitization.universe.maths.physics.Velocity
 import relativitization.universe.utils.RelativitizationLogManager
+import kotlin.math.exp
 
-object ABMFlockingSVMAI : AI() {
+object ABMFlockingDensitySpeedAI : AI() {
     private val logger = RelativitizationLogManager.getLogger()
 
     override fun compute(universeData3DAtPlayer: UniverseData3DAtPlayer): List<Command> {
 
-        val flockSpeed: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
-            .getOrElse("flockSpeed") {
-                logger.error("No flockSpeed defined")
-                0.5
+        val minFlockSpeed: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
+            .getOrElse("minFlockSpeed") {
+                logger.error("No minFlockSpeed defined")
+                0.1
+            }
+
+        val maxFlockSpeed: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
+            .getOrElse("maxFlockSpeed") {
+                logger.error("No maxFlockSpeed defined")
+                0.9
+            }
+
+        val speedDecayFactor: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
+            .getOrElse("speedDecayFactor") {
+                logger.error("No speedDecayFactor defined")
+                0.1
             }
 
         val nearByRadius: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
             .getOrElse("nearByRadius") {
                 logger.error("No nearByRadius defined")
+                3.0
+            }
+
+        val densityNearByRadius: Double = universeData3DAtPlayer.universeSettings.otherDoubleMap
+            .getOrElse("densityNearByRadius") {
+                logger.error("No densityNearByRadius defined")
                 3.0
             }
 
@@ -37,6 +54,14 @@ object ABMFlockingSVMAI : AI() {
                 logger.error("No accelerationFuelFraction defined")
                 1.0
             }
+
+        val flockSpeed: Double = computeFlockSpeed(
+            minFlockSpeed = minFlockSpeed,
+            maxFlockSpeed = maxFlockSpeed,
+            speedDecayFactor = speedDecayFactor,
+            densityNearbyRadius = densityNearByRadius,
+            universeData3DAtPlayer = universeData3DAtPlayer,
+        )
 
         val averageVelocity: Velocity = universeData3DAtPlayer.getPlayerInSphere(nearByRadius)
             .fold(Velocity(0.0, 0.0, 0.0)) { acc, playerData ->
@@ -58,5 +83,22 @@ object ABMFlockingSVMAI : AI() {
         )
 
         return listOf(abmFlockingChangeVelocityCommand)
+    }
+
+    /**
+     * Compute density-dependent flock speed
+     */
+    private fun computeFlockSpeed(
+        minFlockSpeed: Double,
+        maxFlockSpeed: Double,
+        speedDecayFactor: Double,
+        densityNearbyRadius: Double,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+    ): Double {
+        val numNeighbour: Int =
+            universeData3DAtPlayer.getNeighbourInSphere(densityNearbyRadius).size
+
+        return (maxFlockSpeed - minFlockSpeed) * exp(-numNeighbour * speedDecayFactor) +
+                minFlockSpeed
     }
 }
