f57168d2a1b23ecefd17a4288beb86670bb93a26
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-05-09 19:35:25 +0800

    Add FlockingSpeedDensityParameterScan3

---
+++ simulations/src/main/kotlin/relativitization/abm/FlockingSpeedDensityParameterScan3.kt
@@ -0,0 +1,58 @@
+package relativitization.abm
+
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.concat
+import org.jetbrains.kotlinx.dataframe.api.describe
+import org.jetbrains.kotlinx.dataframe.io.writeCSV
+import relativitization.universe.maths.collection.DoubleRange
+import java.io.File
+
+fun main() {
+    val dfList: MutableList<DataFrame<*>> = mutableListOf()
+
+    // range of speed of light
+    val speedRangeList: List<Double> = DoubleRange.computeList(
+        from = 0.0,
+        to = 0.8,
+        step = 0.1,
+        decimalPlace = 1
+    )
+
+    // in radian
+    val maxAnglePerturbationList: List<Double> = DoubleRange.computeList(
+        from = 0.0,
+        to = 2.0,
+        step = 0.4,
+        decimalPlace = 1
+    )
+
+    for (speedRange in speedRangeList) {
+        for (maxAnglePerturbation in maxAnglePerturbationList) {
+            println("Speed diff: $speedRange. Perturbation angle: $maxAnglePerturbation. ")
+            dfList.add(
+                flockingSpeedDensitySingleRun(
+                    numPlayer = 50,
+                    speedOfLight = 1.0,
+                    initialFlockSpeed = 0.9,
+                    minFlockSpeed = 0.9 - speedRange,
+                    maxFlockSpeed = 0.9,
+                    speedDecayFactor = 0.2,
+                    nearbyRadius = 3.0,
+                    densityNearbyRadius = 1.0,
+                    maxAnglePerturbation = maxAnglePerturbation,
+                    accelerationFuelFraction = 1.0,
+                    numStep = 1000,
+                    randomSeed = 100L,
+                    printStep = false
+                )
+            )
+        }
+    }
+
+    val df = dfList.concat()
+
+    println(df.describe())
+
+    File("data").mkdirs()
+    df.writeCSV("./data/flockingSpeedDensityParameterScan3.csv")
+}
