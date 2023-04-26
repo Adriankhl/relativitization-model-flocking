d9531f0df09653489cd3b1c79e6f19bde0630d8e
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-26 22:21:56 +0200

    Fix bash warning

--- run-all-simulations.sh
+++ run-all-simulations.sh
@@ -4,5 +4,5 @@ fileList=$(find ./simulations/src/main/kotlin/relativitization/flocking ! -name 
 
 for fileName in $fileList; do
   className=${fileName::-3}
-  ./gradlew :simulations:run -PmainClass=relativitization.flocking."$className"Kt -PprocessorCount=${1:-10} -PramPercentage=${2:-50}
+  ./gradlew :simulations:run -PmainClass=relativitization.flocking."$className"Kt -PprocessorCount="${1:-10}" -PramPercentage="${2:-50}"
 done
