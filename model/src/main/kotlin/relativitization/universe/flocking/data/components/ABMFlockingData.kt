30649674b7202ec4d3e6fb6a63be9d18fa48868e
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-04-26 20:47:34 +0200

    Use GenerateImmutable annotation to generate immutable data

--- model/src/main/kotlin/relativitization/universe/flocking/data/components/ABMFlockingData.kt
+++ model/src/main/kotlin/relativitization/universe/flocking/data/components/ABMFlockingData.kt
@@ -1,20 +1,12 @@
 package relativitization.universe.flocking.data.components
 
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import ksergen.annotations.GenerateImmutable
 import relativitization.universe.core.data.MutablePlayerInternalData
 import relativitization.universe.core.data.PlayerInternalData
 import relativitization.universe.core.data.components.MutablePlayerDataComponent
-import relativitization.universe.core.data.components.PlayerDataComponent
 
-@Serializable
-@SerialName("ABMFlockingData")
-data class ABMFlockingData(
-    val restMass: Double = 1.0,
-    val restMassFraction: Double = 1.0,
-) : PlayerDataComponent()
-
-@Serializable
+@GenerateImmutable
 @SerialName("ABMFlockingData")
 data class MutableABMFlockingData(
     var restMass: Double = 1.0,
